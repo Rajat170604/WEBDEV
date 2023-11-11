135abe5760bfa1c8f03138f6dd35be36f0fc5b9a
Rajat170604 <rajatkumardalai21@gmail.com>
2023-11-11 18:09:34 +0530

    variable and constant

---
+++ JAVA SCRIPT/2ndclass.js
@@ -0,0 +1,19 @@
+const accountId = 25646235
+let accountEmail = "kjhsvdv@sbdb.cvbk"
+var accountPassword = "354jg%$%"
+accountCity = "Berhampur"
+let accountState;
+
+// accountId = 655431 // Not allowed
+console.log(accountId);
+
+accountEmail = "khkjglh@ksv.njgb"
+accountPassword = "131c31a3sd1c335131"
+accountCity = "lksdfvb"
+
+/*
+prefer not to use var 
+because of block scope and funtional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
