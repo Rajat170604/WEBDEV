117d4eba9a02febd260b947d5aa40b554a99675d
Rajat170604 <rajatkumardalai21@gmail.com>
2023-11-12 08:35:58 +0530

    data types

--- JAVA SCRIPT/3rdclass.js
+++ JAVA SCRIPT/3rdclass.js
@@ -1,3 +1,21 @@
 "use strict"; // treat  all nodejs as newer version
 
 // alert(3+3) // we are using nodejs not web browser.
+
+let name = "Rajat";
+let age = 19;
+let isLoggedin = true;
+let state;
+
+// number=> 2 to the power 53
+// bigInt
+// String=> ""
+// Boolean=> true/false
+// null=> standalone value
+// undefined
+// symbol=> unique
+// object
+
+console.table([typeof name, typeof age, typeof isLoggedin, typeof state, typeof null, typeof undefined])
+// type of undefined is undefined
+// type of null is object
